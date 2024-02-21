688a13c38a5d50ede2443117051c9aa922ae897a
cpeterp <cppampalone@email.wm.edu>
2024-02-21 08:53:11 -0500

    outline for gym

---
+++ run/tetris_gym.py
@@ -0,0 +1,165 @@
+import io
+from pathlib import Path
+import random
+from typing import Any, SupportsFloat, Tuple, Dict, List
+
+from gymnasium import Env, spaces
+from gymnasium.core import RenderFrame
+import numpy as np
+from pyboy import PyBoy, WindowEvent
+from pyboy.botsupport.tilemap import TileMap
+
+import common
+
+
+class TetrisGymEnv(Env):
+    def __init__(self, config: dict):
+        """Main Gymnasium environment for running Tetris. The config options
+        allow us to use this for training and for playing back runs in a human-
+        readable format"""
+        run_headless = config["run_headless"]
+
+        self.valid_actions = [
+            WindowEvent.PRESS_ARROW_DOWN,
+            WindowEvent.PRESS_ARROW_LEFT,
+            WindowEvent.PRESS_ARROW_RIGHT,
+            WindowEvent.PRESS_BUTTON_A,
+            WindowEvent.PRESS_BUTTON_B,
+        ]
+        self.valid_releases = [
+            WindowEvent.RELEASE_ARROW_DOWN,
+            WindowEvent.RELEASE_ARROW_LEFT,
+            WindowEvent.RELEASE_ARROW_RIGHT,
+            WindowEvent.RELEASE_BUTTON_A,
+            WindowEvent.RELEASE_BUTTON_B,
+        ]
+
+        self.starting_states = self.load_starting_states()
+
+        self.output_shape = (18, 20, 3)
+        self.frame_stacks = 2
+        self.col_steps = 16
+        self.output_full = (
+            # Stacking consecutive input frames permits memory for the agent
+            self.output_shape[0] * self.frame_stacks,
+            self.output_shape[1],
+            self.output_shape[2],
+        )
+
+        # Set these in ALL subclasses
+        self.action_space = spaces.Discrete(len(self.valid_actions))
+        self.observation_space = spaces.Box(
+            low=0, high=255, shape=self.output_full, dtype=np.uint8
+        )
+
+        window_type = "headless" if run_headless else "SDL2"
+
+        self.pb = PyBoy(
+            str(common.TETRIS_ROM_PATH),
+            window_type=window_type,
+            debugging=False,
+            disable_input=True,
+        )
+        self.botsupport_manager = self.pb.botsupport_manager()
+        self.pb.set_emulation_speed(
+            0 if run_headless else config["visual_playback_speed"]
+        )
+
+    def reset(
+        self, seed: int | None = None, options: Dict[str, Any] | None = None
+    ) -> Tuple[Any | Dict[str, Any]]:
+        # Use seed to select a random starting state from the directory
+        self.seed = seed
+        random(seed)
+        starting_state_i = random.randint(0, len(self.starting_states) - 1)
+        starting_state = self.starting_states[starting_state_i]
+        self.pb.load_state(starting_state)
+
+        # Set default values
+        self.score = 0
+        self.lines = 0
+        self.level = 0
+        self.top_line = 0
+        # TODO: Define functions to pull these from memory
+        self.current_shape = None
+        self.next_shape = None
+
+        # TODO: Need to set up recording here to replay attempts
+
+        # Grab the initial observation
+        observation = self.render()
+        additional_info = {}  # TBD
+        return observation, additional_info
+
+    def step(
+        self, action: Any
+    ) -> Tuple[Any, SupportsFloat, bool, bool, Dict[str, Any]]:
+        self.run_action(action)
+
+        observation = self.render()
+
+        # Determine reward
+        new_score = self.get_score()
+        if new_score >= self.score:
+            score_diff = self.score - new_score
+        else:
+            # The bits rolled over
+            # TODO Issue warning/note/log if this happens
+            score_diff = (999999 - self.score) + new_score
+        self.score = new_score
+        reward = score_diff
+
+        return observation, reward, terminated, truncated, additional_info
+
+    def render(
+        self,
+        reduce_res: bool = True,
+        add_memory: bool = True,
+        update_mem: bool = True,
+    ) -> RenderFrame | List[RenderFrame] | None:
+        background_map = self.botsupport_manager.tilemap_background()
+        background_map = self.botsupport_manager.tilemap_background()
+        if reduce_res:
+            pass
+        return game_pixels_render
+
+    def run_action(self, action):
+        return None
+
+    def load_starting_states(self) -> List[io.BytesIO]:
+        """Loads all starting states as BytesIO that can be read by PyBoy"""
+        starting_states = []
+        for fp in common.POST_START_STATE_DIR.glob("*.state"):
+            with open(fp, "rb") as B:
+                starting_state_bytes = B.read()
+                B.close()
+            starting_states.append(io.BytesIO(starting_state_bytes))
+        return starting_states
+
+    def get_score(self) -> int:
+        """Returns the current score of the game.
+
+        Tetris records its score as a 3 byte little endian BCD starting at
+        0xC0A0. Each byte stores a two digit decimal number, starting with 00-
+        99.
+
+        For example, the bytes 0x55 0x03 0x01 correspond to a score of 10355"""
+        score_hex_values = [
+            self.pb.get_memory_value(0xC0A0),
+            self.pb.get_memory_value(0xC0A1),
+            self.pb.get_memory_value(0xC0A2),
+        ]
+        score_decimal_value = (
+            score_hex_values[0] % 16
+            + (score_hex_values[0] // 16) * 10
+            + (score_hex_values[1] % 16) * 100
+            + (score_hex_values[1] // 16) * 1000
+            + (score_hex_values[2] % 16) * 10000
+            + (score_hex_values[2] // 16) * 100000
+        )
+        return score_decimal_value
+
+
+if __name__ == "__main__":
+    config = {"run_headless": True, "visual_playback_speed": 5}
+    tge = TetrisGymEnv(config)
---
+++ run/tile_lookup.py
@@ -0,0 +1,6 @@
+"""
+Tile IDs exposed during main game. Referenced by PyBoy.botsupport.tilemap.TileMap 
+"""
+
+BLANK_TILE = 47
+SHAPE_TILES = [n for n in range(128, 143 + 1)]
